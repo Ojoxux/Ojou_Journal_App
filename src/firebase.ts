ddd5d6ce14ca5598ca601a27ac0d4cc2c7a8831f
Jou Okuyama <ojoxux@gmail.com>
2024-09-27 20:40:13 +0900

    feat: 🔧 Firebase設定を環境変数で更新

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDrqEnyBJk9A18gwhaEatUAwY6K4O4rqwg',
-  authDomain: 'ojou-jounal.firebaseapp.com',
-  projectId: 'ojou-jounal',
-  storageBucket: 'ojou-jounal.appspot.com',
-  messagingSenderId: '167287675906',
-  appId: '1:167287675906:web:4d0948baede2edf9af5b1b',
-  measurementId: 'G-8ZVLV883SR',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
